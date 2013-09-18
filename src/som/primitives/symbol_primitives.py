3468a726e044726d3656210bc0e2ef354023326a
Stefan Marr <git@stefan-marr.de>
2013-09-18 10:35:38 +0200

    Added SymbolPrimitives
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/primitives/symbol_primitives.py
@@ -0,0 +1,10 @@
+from som.primitives.primitives import Primitives
+from som.vmobjects.primitive   import Primitive
+
+class SymbolPrimitives(Primitives):
+    
+    def install_primitives(self):
+        def _asString(self, frame, interpreter):
+            rcvr = frame.pop()
+            frame.push(self._universe.new_string(rcvr.get_string()))
+        self._install_instance_primitive(Primitive("asString", self._universe, _asString))
