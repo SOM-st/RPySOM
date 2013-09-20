SOM-st/RPySOM

commit 1daff8b188e914414f7d3471fde19fd45f475928

RPython chokes on dicts with values of different types
this is a problem in the case when we want to "as_str" the pseudo-enum
. So we just filter the int-value part of the dict and, as a small optimization
use a translate-time unrolling of the if-switch.

src/som/compiler/symbol.py
@@ -1,3 +1,5 @@
+from rpython.rlib.unroll import unrolling_iterable
+
 # Symbol is a 'lightweight' enum, in Python 3.4, we could use Enum as superclass
 class Symbol(object):
     NONE             = -1
@@ -33,9 +35,16 @@ class Symbol(object):
     KeywordSequence  = 29
     OperatorSequence = 30
 
-
 def symbol_as_str(symbol):
-    for key, val in Symbol.__dict__:
+    symbols = unrolling_iterable(int_constants_of(Symbol))
+    for (key, val) in symbols:
         if val == symbol:
             return key
-    raise ValueError('No Symbol defined for the value %d.' % symbol)
+            raise ValueError('No Symbol defined for the value %d.' % symbol)
+
+def int_constants_of(cls):
+    out = {}
+    for key, value in cls.__dict__.items():
+        if isinstance(value, int):
+            out[key] = value
+    return out

src/som/interpreter/bytecodes.py
@@ -1,3 +1,5 @@
+from rpython.rlib.unroll import unrolling_iterable
+
 class Bytecodes(object):
     
     # Bytecodes used by the Simple Object Machine (SOM)
@@ -76,8 +78,16 @@ def bytecode_as_str(bytecode):
     if not isinstance(bytecode, int):
         raise ValueError('bytecode is expected to be an integer.')
     
-    for key, val in Bytecodes.__dict__.iteritems():
+    bytecodes = unrolling_iterable(int_constants_of(Bytecodes))
+    for key, val in bytecodes:
         if val == bytecode:
             return key.upper()
         
     raise ValueError('No defined defined for the value %d.' % bytecode)
+
+def int_constants_of(cls):
+    out = {}
+    for key, value in cls.__dict__.items():
+        if isinstance(value, int):
+            out[key] = value
+    return out

commit 58dfd70ca8539edb070537aee2d48bf0d20a8b1d

Corrected indentation
- issue was introduced by 1daff8b188e914414f7d3471fde19fd45f475928

Signed-off-by: Stefan Marr <git@stefan-marr.de>

src/som/compiler/symbol.py
@@ -40,7 +40,7 @@ def symbol_as_str(symbol):
     for (key, val) in symbols:
         if val == symbol:
             return key
-            raise ValueError('No Symbol defined for the value %d.' % symbol)
+    raise ValueError('No Symbol defined for the value %d.' % symbol)
 
 def int_constants_of(cls):
     out = {}