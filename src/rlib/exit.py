eb733661863874db95cdf04db1993d6fa3112052
Stefan Marr <git@stefan-marr.de>
2013-09-21 21:41:59 +0200

    Don't call the BaseException constructor, RPython complains
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/rlib/exit.py
+++ src/rlib/exit.py
@@ -4,5 +4,4 @@ class Exit(BaseException):
     We don't use sys.exit because it is a little problematic with RPython.
     """
     def __init__(self, code):
-        BaseException.__init__(self)
         self.code = code
