SOM-st/RPySOM

commit b8d3cc26b4f16e615c1f83b2f7fd30426c0037ed

we cannot use the stdlib os.path.split* so we do our own

src/som/vm/universe.py
@@ -171,9 +171,18 @@ class Universe(object):
     # take argument of the form "../foo/Test.som" and return
     # "../foo", "Test", "som"
     def _get_path_class_ext(self, path):
-        (path, file_name) = os.path.split(path)
-        (file_name, ext)  = os.path.splitext(file_name)
-        return (path, file_name, ext[1:])
+        path_and_file = path.rsplit(os.pathsep, 1)
+        if len(path_and_file) <= 1:
+            path = ""
+        else:
+            path = path_and_file[0]
+        file_and_ext  = path_and_file[-1].rsplit(".", 1)
+        if len(file_and_ext) <= 1:
+            ext = ""
+        else:
+            ext = file_and_ext[-1]
+        file_name = file_and_ext[0]
+        return (path, file_name, ext)
     
     def _print_usage_and_exit(self):
         # Print the usage

commit bc5bb8a29963445e69684a223f102a4348875e56

Fixed use of wrong separator in path splitting routine
Bug introduced in b8d3cc26b4f16e615c1f83b2f7fd30426c0037ed, which was replacing os.path.split with RPython compatible equivalent

Signed-off-by: Stefan Marr <git@stefan-marr.de>

src/som/vm/universe.py
@@ -178,7 +178,7 @@ class Universe(object):
     # take argument of the form "../foo/Test.som" and return
     # "../foo", "Test", "som"
     def _get_path_class_ext(self, path):
-        path_and_file = path.rsplit(os.pathsep, 1)
+        path_and_file = path.rsplit(os.sep, 1)
         if len(path_and_file) <= 1:
             path = ""
         else: