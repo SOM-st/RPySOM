e59a8e6a55ecaeb3b40d8cb9f442ed8a7e066b93
Stefan Marr <git@stefan-marr.de>
2013-11-08 22:17:54 +0100

    Make som.sh callable from other directories
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- som.sh
+++ som.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+DIR="`dirname \"$0\"`"
 if [ -z "$PYTHON" ]; then
-  PYTHON=python
+  PYTHON=pypy
 fi
-PYTHONPATH=src:$PYTHONPATH
-exec $PYTHON src/som/vm/universe.py "$@"
+export PYTHONPATH=$DIR/src:$DIR/pypy:$PYTHONPATH
+exec $PYTHON $DIR/src/som/vm/universe.py "$@"
