25b722d24c72d7d3190dae4d2fcf061f4975a0cb
Stefan Marr <git@stefan-marr.de>
2014-03-01 09:43:20 +0100

    Added `Invokable` node
    This one is currently not strictly necessary, because it only encodes the parameter passing with Frame objects.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/som/interpreter/invokable.py
@@ -0,0 +1,21 @@
+from rtruffle.node import Node
+
+from .frame import Frame
+
+
+class Invokable(Node):
+
+    _immutable_fields_ = ['_expr_or_sequence?', '_universe', '_number_of_temps']
+    _child_nodes_      = ['_expr_or_sequence']
+
+    def __init__(self, source_section, expr_or_sequence, number_of_temps,
+                 universe):
+        Node.__init__(self, source_section)
+        self._expr_or_sequence = self.adopt_child(expr_or_sequence)
+        self._universe         = universe
+        self._number_of_temps  = number_of_temps
+
+    def invoke(self, caller_frame, receiver, arguments):
+        frame = Frame(receiver, arguments, self._number_of_temps,
+                      caller_frame, self._universe.nilObject)
+        return self._expr_or_sequence.execute(frame)
