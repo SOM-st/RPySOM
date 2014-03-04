e229720efdf93ffe1ad04c7cf8efa24f562ca027
Stefan Marr <git@stefan-marr.de>
2014-03-04 10:49:54 +0100

    Experiment with another merge_point in invobable
    Doesn't work any better :-(
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/invokable.py
+++ src/som/interpreter/invokable.py
@@ -1,6 +1,17 @@
+# from rpython.rlib import jit
 from rtruffle.node import Node
 
 from .frame import Frame
+
+
+# jitdriver = jit.JitDriver(
+#     greens=['self', 'receiver'],
+#     reds= ['arguments', 'caller_frame'])
+#     # virtualizables=['caller_frame'])
+#     # ,
+#     # get_printable_location=get_printable_location,
+#     # should_unroll_one_iteration = lambda method: True)
+
 
 
 class Invokable(Node):
@@ -16,6 +27,11 @@ class Invokable(Node):
         self._number_of_temps  = number_of_temps
 
     def invoke(self, caller_frame, receiver, arguments):
+        # jitdriver.jit_merge_point(self      = self,
+        #                           receiver  = receiver,
+        #                           arguments = arguments,
+        #                           caller_frame = caller_frame)
+
         frame = Frame(receiver, arguments, self._number_of_temps,
                       caller_frame, self._universe.nilObject)
         return self._expr_or_sequence.execute(frame)
