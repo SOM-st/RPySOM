0edb9b5ba8b2059329a5d97a1e3fefe77ae53d9e
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-03-04 12:34:55 +0100

    enable tracing of methods

--- src/som/interpreter/invokable.py
+++ src/som/interpreter/invokable.py
@@ -1,16 +1,27 @@
-# from rpython.rlib import jit
+from rpython.rlib import jit
 from rtruffle.node import Node
 
 from .frame import Frame
 
+def get_printable_location(invokable):
+    return invokable._source_section._identifier
 
-# jitdriver = jit.JitDriver(
-#     greens=['self', 'receiver'],
-#     reds= ['arguments', 'caller_frame'])
-#     # virtualizables=['caller_frame'])
-#     # ,
-#     # get_printable_location=get_printable_location,
-#     # should_unroll_one_iteration = lambda method: True)
+jitdriver = jit.JitDriver(
+     greens=['self'],
+     reds= ['arguments', 'caller_frame', 'receiver'],
+     # virtualizables=['caller_frame'])
+      get_printable_location=get_printable_location,
+
+     # the next line is a workaround around a likely bug in RPython
+     # for some reason, the inlining heuristics default to "never inline" when
+     # two different jit drivers are involved (in our case, the primitive
+     # driver, and this one).
+
+     # the next line says that calls involving this jitdriver should always be
+     # inlined once (which means that things like Integer>>< will be inlined
+     # into a while loop again, when enabling this driver).
+     should_unroll_one_iteration = lambda self: True,
+     )
 
 
 
@@ -27,10 +38,10 @@ class Invokable(Node):
         self._number_of_temps  = number_of_temps
 
     def invoke(self, caller_frame, receiver, arguments):
-        # jitdriver.jit_merge_point(self      = self,
-        #                           receiver  = receiver,
-        #                           arguments = arguments,
-        #                           caller_frame = caller_frame)
+        jitdriver.jit_merge_point(self      = self,
+                                  receiver  = receiver,
+                                  arguments = arguments,
+                                  caller_frame = caller_frame)
 
         frame = Frame(receiver, arguments, self._number_of_temps,
                       caller_frame, self._universe.nilObject)
