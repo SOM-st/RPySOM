8b8821bddaf40421fe49bdd792728c6778d21ce0
Stefan Marr <git@stefan-marr.de>
2014-03-24 18:11:40 +0100

    Make objects always have 5 slots and extension optional array
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/object.py
+++ src/som/vmobjects/object.py
@@ -7,11 +7,24 @@ class Object(AbstractObject):
     
     # Static field indices and number of object fields
     NUMBER_OF_OBJECT_FIELDS = 0
+    
+    NUMBER_OF_DIRECT_FIELDS = 5
 
     def __init__(self, nilObject, number_of_fields = -1, obj_class = None):
         num_fields = (number_of_fields if number_of_fields != -1
                       else self._get_default_number_of_fields())
-        self._fields = [nilObject] * num_fields
+        
+        self._field1 = nilObject
+        self._field2 = nilObject
+        self._field3 = nilObject
+        self._field4 = nilObject
+        self._field5 = nilObject
+        
+        if num_fields >= self.NUMBER_OF_DIRECT_FIELDS:
+            self._fields = [nilObject] * (num_fields - self.NUMBER_OF_DIRECT_FIELDS)
+        else:
+            self._fields = []
+             
         self._class = obj_class or nilObject
 
     def get_class(self, universe):
@@ -39,10 +52,22 @@ class Object(AbstractObject):
     def get_field(self, index):
         # Get the field with the given index
         assert isinstance(index, int)
-        return self._fields[index]
+        
+        if index == 0: return self._field1
+        if index == 1: return self._field2
+        if index == 2: return self._field3
+        if index == 3: return self._field4
+        if index == 4: return self._field5
+        return self._fields[index - self.NUMBER_OF_DIRECT_FIELDS]
   
     def set_field(self, index, value):
         # Set the field with the given index to the given value
         assert isinstance(index, int)
         assert isinstance(value, AbstractObject)
-        self._fields[index] = value
+        
+        if index == 0: self._field1 = value; return
+        if index == 1: self._field2 = value; return
+        if index == 2: self._field3 = value; return
+        if index == 3: self._field4 = value; return
+        if index == 4: self._field5 = value; return
+        self._fields[index - self.NUMBER_OF_DIRECT_FIELDS] = value
