1089c383f3733bbb89f6e2a99e2ae1a69690d7fe
Stefan Marr <git@stefan-marr.de>
2014-04-13 12:07:18 +0200

    Minor style cleanup
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/compiler/symbol.py
+++ src/som/compiler/symbol.py
@@ -39,10 +39,9 @@ def _sorted_symbols(cls):
     """This function is only called a single time, at load time of this module.
        For RPypthon, this means, during translation of the module.
     """
-    return [key for value, key in \
-            sorted([(value, key) for key, value in cls.__dict__.items()]) \
-            if isinstance(value, int)
-    ]
+    return [key for value, key in
+            sorted([(value, key) for key, value in cls.__dict__.items()])
+            if isinstance(value, int)]
 _symbols = _sorted_symbols(Symbol)
 
 
