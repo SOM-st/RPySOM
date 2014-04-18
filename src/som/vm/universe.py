a877eee1b9e4dcb801f93b7f29a59a96f78a2f78
Stefan Marr <git@stefan-marr.de>
2014-04-18 19:10:43 +0200

    Increase trace length for testing
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vm/universe.py
+++ src/som/vm/universe.py
@@ -70,6 +70,7 @@ class Universe(object):
             "_globals"]
 
     def __init__(self, avoid_exit = False):
+        jit.set_param(None, 'trace_limit', 6000 * 100)
         self._symbol_table   = {}
         self._globals        = {}
 
