61469328951b70ac804d3df274f597b7478ce92c
Stefan Marr <git@stefan-marr.de>
2014-04-26 20:52:58 +0200

    make sure BigInteger values are int or long
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/biginteger.py
+++ src/som/vmobjects/biginteger.py
@@ -1,11 +1,13 @@
 from som.vmobjects.abstract_object import AbstractObject
 
+
 class BigInteger(AbstractObject):
 
     _immutable_fields_ = ["_embedded_biginteger"]
 
     def __init__(self, value):
         AbstractObject.__init__(self)
+        assert isinstance(value, long) or isinstance(value, int)
         self._embedded_biginteger = value
     
     def get_embedded_biginteger(self):
