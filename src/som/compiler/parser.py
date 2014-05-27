5d971c9cb0ddd612793031282d07121d39150a50
Stefan Marr <git@stefan-marr.de>
2014-05-27 21:57:22 +0200

    Added support for parsing big integer literals
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/compiler/parser.py
+++ src/som/compiler/parser.py
@@ -1,3 +1,5 @@
+from rpython.rlib.rbigint import rbigint
+from rpython.rlib.rstring import ParseStringOverflowError
 from rtruffle.source_section import SourceSection
 
 from ..interpreter.nodes.block_node       import BlockNode, BlockNodeWithContext
@@ -528,12 +530,18 @@ class Parser(object):
             i = int(self._text)
             if negate_value:
                 i = 0 - i
+            result = self._universe.new_integer(i)
+        except ParseStringOverflowError:
+            bigint = rbigint.fromstr(self._text)
+            if negate_value:
+                bigint.sign = -1
+            result = self._universe.new_biginteger(bigint)
         except ValueError:
             raise ParseError("Could not parse integer. "
                              "Expected a number but got '%s'" % self._text,
                              Symbol.NONE, self)
         self._expect(Symbol.Integer)
-        return LiteralNode(self._universe.new_integer(i))
+        return LiteralNode(result)
 
     def _literal_double(self, negate_value):
         try:
