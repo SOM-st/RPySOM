ada4b831eb8d99aeb741c83cc3b9adcd843f7c65
Stefan Marr <git@stefan-marr.de>
2014-09-23 18:13:25 +0200

    Added #invokeOn:with: primitive
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/primitive_primitives.py
+++ src/som/primitives/primitive_primitives.py
@@ -1,4 +1,6 @@
 from som.primitives.primitives import Primitives
+from som.vmobjects.abstract_object import AbstractObject
+from som.vmobjects.array       import Array
 from som.vmobjects.primitive   import Primitive 
 
 
@@ -10,9 +12,23 @@ def _signature(ivkbl, rcvr, args):
     return rcvr.get_signature()
 
 
+def _invoke_on_with(ivkbl, rcvr, args):
+    assert isinstance(rcvr,    Primitive)
+    assert isinstance(args[0], AbstractObject)
+    assert isinstance(args[1], Array) or args[1] is ivkbl.get_universe().nilObject
+
+    if args[1] is ivkbl.get_universe().nilObject:
+        direct_args = []
+    else:
+        direct_args = args[1].get_indexable_fields()
+    return rcvr.invoke(args[0], direct_args)
+
+
 class PrimitivePrimitives(Primitives):
     def install_primitives(self):
         self._install_instance_primitive(Primitive("holder",
                                                    self._universe, _holder))
         self._install_instance_primitive(Primitive("signature",
                                                    self._universe, _signature))
+        self._install_instance_primitive(Primitive("invokeOn:with:",
+                                                   self._universe, _invoke_on_with))
