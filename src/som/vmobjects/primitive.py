83ba6638997b2625eb5367b77e85f958da781bc4
Stefan Marr <git@stefan-marr.de>
2014-09-23 18:11:06 +0200

    Rename _invoke to _prim_fun
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/primitive.py
+++ src/som/vmobjects/primitive.py
@@ -2,14 +2,14 @@ from som.vmobjects.abstract_object    import AbstractObject
 
 
 class Primitive(AbstractObject):
-    _immutable_fields_ = ["_invoke", "_is_empty", "_signature", "_holder",
+    _immutable_fields_ = ["_prim_fun", "_is_empty", "_signature", "_holder",
                           "_universe"]
         
-    def __init__(self, signature_string, universe, invoke, is_empty=False):
+    def __init__(self, signature_string, universe, prim_fun, is_empty = False):
         AbstractObject.__init__(self)
         
         self._signature = universe.symbol_for(signature_string)
-        self._invoke    = invoke
+        self._prim_fun  = prim_fun
         self._is_empty  = is_empty
         self._holder    = None
         self._universe  = universe
@@ -18,7 +18,7 @@ class Primitive(AbstractObject):
         return self._universe
 
     def invoke(self, rcvr, args):
-        inv = self._invoke
+        inv = self._prim_fun
         return inv(self, rcvr, args)
 
     def invoke_void(self, rcvr, args):
