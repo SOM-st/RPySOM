746a83c34f1b0dbbd22ea4907fee6f33e0010241
Stefan Marr <git@stefan-marr.de>
2014-09-27 23:19:51 +0200

    Added dispatch chains for method lookup
    This should speed up the interpreter.
    On DeltaBlue it gives about 10%.
    Note, there is something going wrong with the meta tracing. So, the dispatch chains are only used for interpretation.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/message_node.py
+++ src/som/interpreter/nodes/message_node.py
@@ -1,7 +1,9 @@
 from rpython.rlib.debug import make_sure_not_resized
-from rpython.rlib.jit   import unroll_safe
+from rpython.rlib.jit   import unroll_safe, we_are_jitted
 
 from .expression_node import ExpressionNode
+from som.interpreter.nodes.dispatch import UninitializedDispatchNode, \
+    SuperDispatchNode
 
 from .specialized.down_to_do_node import IntDownToIntDoNode, \
     IntDownToDoubleDoNode
@@ -71,6 +73,23 @@ class UninitializedMessageNode(AbstractMessageNode):
 
 class GenericMessageNode(AbstractMessageNode):
 
+    _immutable_fields_ = ['_dispatch?']
+    _child_nodes_      = ['_dispatch']
+
+    def __init__(self, selector, universe, rcvr_expr, arg_exprs,
+                 source_section = None):
+        AbstractMessageNode.__init__(self, selector, universe, rcvr_expr,
+                                     arg_exprs, source_section)
+        if rcvr_expr.is_super_node():
+            dispatch = SuperDispatchNode(selector, rcvr_expr.get_super_class(),
+                                         universe)
+        else:
+            dispatch = UninitializedDispatchNode(selector, universe)
+        self._dispatch = self.adopt_child(dispatch)
+
+    def replace_dispatch_list_head(self, node):
+        self._dispatch.replace(node)
+
     def execute(self, frame):
         rcvr, args = self._evaluate_rcvr_and_args(frame)
         return self.execute_evaluated(frame, rcvr, args)
@@ -80,24 +99,40 @@ class GenericMessageNode(AbstractMessageNode):
         self.execute_evaluated_void(frame, rcvr, args)
 
     def execute_evaluated_void(self, frame, rcvr, args):
+        assert frame is not None
+        assert rcvr is not None
         assert args is not None
         make_sure_not_resized(args)
-        method = self._lookup_method(rcvr)
-        if method:
-            method.invoke_void(rcvr, args)
+        if we_are_jitted():
+            self._direct_dispatch_void(rcvr, args)
         else:
-            rcvr.send_does_not_understand_void(self._selector, args,
-                                               self._universe)
+            self._dispatch.execute_dispatch_void(rcvr, args)
 
     def execute_evaluated(self, frame, rcvr, args):
+        assert frame is not None
+        assert rcvr is not None
         assert args is not None
         make_sure_not_resized(args)
+        if we_are_jitted():
+            return self._direct_dispatch(rcvr, args)
+        else:
+            return self._dispatch.execute_dispatch(rcvr, args)
+
+    def _direct_dispatch(self, rcvr, args):
         method = self._lookup_method(rcvr)
         if method:
             return method.invoke(rcvr, args)
         else:
             return rcvr.send_does_not_understand(self._selector, args,
                                                  self._universe)
+
+    def _direct_dispatch_void(self, rcvr, args):
+        method = self._lookup_method(rcvr)
+        if method:
+            method.invoke_void(rcvr, args)
+        else:
+            rcvr.send_does_not_understand_void(self._selector, args,
+                                               self._universe)
 
     def _lookup_method(self, rcvr):
         rcvr_class = self._class_of_receiver(rcvr)
