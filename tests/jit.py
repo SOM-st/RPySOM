9d33ca5609f097b642e4d47fc3132c45e810737c
Stefan Marr <git@stefan-marr.de>
2014-09-30 12:59:51 +0200

    Use preinitialized universe in jit.py
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/jit.py
+++ tests/jit.py
@@ -14,7 +14,7 @@ class o:
 conftest.option = o
 from rpython.jit.metainterp.test.test_ajit import LLJitMixin
 
-from som.vm.universe import Universe, Exit
+from som.vm.universe import get_current, Exit
 import som.compiler.sourcecode_compiler as sourcecode_compiler
 
 
@@ -24,7 +24,7 @@ cp = py.path.local(__file__).dirpath().dirpath().join("Smalltalk").strpath
 class TestLLtype(LLJitMixin):
 
     def _compile_and_lookup(self, source, start, classpath):
-        u = Universe()
+        u = get_current()
         u.setup_classpath(classpath)
         u._initialize_object_system()
         cls = sourcecode_compiler.compile_class_from_string(source, None, u)
