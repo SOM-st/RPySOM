35a8f1738d12a3d75bca44181966c3e463caef34
Stefan Marr <git@stefan-marr.de>
2020-08-26 22:46:26 +0100

    Correct path for primitives and add error handling for primitives
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/primitives/known.py
+++ src/som/primitives/known.py
@@ -6,6 +6,8 @@ from ..interp_type import is_ast_interpreter, is_bytecode_interpreter
    time with RPython.
 """
 
+EXPECTED_NUMBER_OF_PRIMITIVE_FILES = 11
+
 
 class PrimitivesNotFound(Exception): pass
 
@@ -28,10 +30,14 @@ def _setup_primitives():
     base_package = "som.primitives."
     if is_ast_interpreter():
         base_package += 'ast.'
-    if is_bytecode_interpreter():
+        interp_dir = 'ast'
+    elif is_bytecode_interpreter():
         base_package += 'bc.'
+        interp_dir = 'bc'
+    else:
+        interp_dir = ''
 
-    directory = py.path.local(__file__).dirpath()
+    directory = py.path.local(__file__).dirpath(interp_dir)
     files = filter(lambda ent: ent.basename.endswith("_primitives.py"),
                    directory.listdir())
     mods = map(lambda mod: import_module(base_package + mod.purebasename),
@@ -41,8 +47,18 @@ def _setup_primitives():
     all_members = reduce(lambda all, each: all + each, all_members)
     all_prims = filter(_is_primitives_class, all_members)
     prim_pairs = map(lambda (name, cls):
-                (name[:name.find("Primitives")], cls),
-                all_prims)
+                     (name[:name.find("Primitives")], cls), all_prims)
+
+    if EXPECTED_NUMBER_OF_PRIMITIVE_FILES != len(prim_pairs):
+        print ""
+        print "SOM PRIMITIVE DISCOVERY: following primitives found:"
+        for name, clazz in prim_pairs:
+            print "   - %s" % name
+        print "Expected number of primitive files: %d, found %d" % (
+            EXPECTED_NUMBER_OF_PRIMITIVE_FILES, len(prim_pairs))
+        print "ERROR: did not find the expected number of primitive files!"
+        import sys
+        sys.exit(1)
     return prim_pairs
 
 
