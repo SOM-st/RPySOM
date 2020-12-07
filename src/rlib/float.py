2c3d41803ec0661db3b9a9a28d75b74b26466f65
Stefan Marr <git@stefan-marr.de>
2020-12-07 18:06:34 +0000

    Implement round_double
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/rlib/float.py
+++ src/rlib/float.py
@@ -1,3 +1,5 @@
+import math
+
 INFINITY = 1e200 * 1e200
 
 try:
@@ -11,4 +13,19 @@ except ImportError:
     def float_to_str(value):
         return str(value)
 
-    round_double = round
+    def round_double(x, ndigits):
+        # round() from libm, which is not available on all platforms!
+        # This version rounds away from zero.
+        absx = abs(x)
+        r = math.floor(absx + 0.5)
+        if r - absx < 1.0:
+            return math.copysign(r, x)
+        else:
+            # 'absx' is just in the wrong range: its exponent is precisely
+            # the one for which all integers are representable but not any
+            # half-integer.  It means that 'absx + 0.5' computes equal to
+            # 'absx + 1.0', which is not equal to 'absx'.  So 'r - absx'
+            # computes equal to 1.0.  In this situation, we can't return
+            # 'r' because 'absx' was already an integer but 'r' is the next
+            # integer!  But just returning the original 'x' is fine.
+            return x
