42c600247c9423431b9652409738a11dfb816d94
Stefan Marr <git@stefan-marr.de>
2020-12-08 13:47:25 +0000

    Fix whitespace
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/rlib/osext.py
+++ src/rlib/osext.py
@@ -1,5 +1,6 @@
 import os
 
+
 def path_split(path):
     """This is a replacement for the combined use of os.path.split and
        os.path.splitext to decompose a relative path into its components.
@@ -17,6 +18,7 @@ def path_split(path):
     file_name = file_and_ext[0]
     return path, file_name, ext
 
+
 def _read_raw(answer):
     buf = os.read(1, 32)
     if len(buf) == 0:
@@ -25,6 +27,7 @@ def _read_raw(answer):
         return answer + buf[:-1], False
     else:
         return answer + buf, True
+
 
 def raw_input(msg = ""):
     os.write(1, msg)
